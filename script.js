66450c74e17146bf32ce94fd1c6f2dd09dedf81d
Martini <paltaguac@gmail.com>
2022-04-28 19:02:54 -0400

    basic operations

---
+++ script.js
@@ -0,0 +1,18 @@
+function sum(a,b){
+    return a+b
+    //return args.reduce((a , b) => a + b , 0)
+}
+function substract(a,b){
+    return a-b
+    //return args.reduce((a , b) => a - b , 0)
+}
+function multiply(a,b){
+    return a*b
+    //return args.reduce((accumulator, currentValue)=>accumulator*currentValue,1)
+}
+function divide(a,b){
+    return a/b
+}
+function operate(operator,a,b){
+    
+}
